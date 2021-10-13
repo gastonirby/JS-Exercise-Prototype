ab65ab5cf546c52bc386ccca746a83b17c63b397
Gaston Irby III <gaston@gastons-imac.lan>
2021-10-13 15:27:24 -0700

    met MVP & completed stretch

--- index.js
+++ index.js
@@ -39,12 +39,45 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
 
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
 
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
+const james = new Person('James', 31);
+const sam = new Person('Sam', 37);
+const latoya = new Person('Latoya', 32);
+
+// console.log(james.toString());
+// console.log(sam.toString());
+// console.log(latoya.toString());
+
+// james.eat('pizza');
+// james.eat('taco');
+// james.eat('candy');
+// james.eat('cake');
+// james.eat('sushi');
+// james.eat('popsicle');
+
+// console.log('James stomach', james.stomach);
+
+// console.log('James pooped. ',james.poop());
+// console.log("James stomach after poopin",james.stomach);
 
 
 
@@ -63,10 +96,32 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+ }
+
+Car.prototype.drive = function(distance) {
+  let driveable = this.tank * this.milesPerGallon
+  if(driveable >= distance){
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - (distance/ this.milesPerGallon);
+  }
+  else{ 
+    this.tank = 0;
+    this.odometer = this.odometer + driveable;
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+}
+
+
+ 
 
 /*
   TASK 3
@@ -75,18 +130,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+}
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function (){
+  return`Playing with ${this.favoriteToy}`;
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. encapsulation
+  2. abstraction
+  3. inheritance
+  4. polymorphism
 */
 
 
